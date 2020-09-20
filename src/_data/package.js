270b3647add2c6362ac47201bdd0407cad4ce936
Pixelastic <tim@pixelastic.com>
2020-09-20 23:38:07 +0200

    fix(styling): Allow for child websites to override theming

--- src/_data/package.js
+++ src/_data/package.js
@@ -1,14 +1,22 @@
 const readJson = require('firost/readJson');
 const parseRepoUrl = require('parse-github-repo-url');
-// const gitRoot = require('firost/gitRoot');
+const firostError = require('firost/error');
 
 module.exports = async (config) => {
   const packagePath = config.rootPath('../lib/package.json');
   const packageContent = await readJson(packagePath);
 
-  const { version, homepage, name, description } = packageContent;
-  const [userName, projectName] = parseRepoUrl(packageContent.repository);
-  const github = `https://github.com/${userName}/${projectName}`;
+  const { version, homepage, name, description, repository } = packageContent;
+  let github;
+  try {
+    const [userName, projectName] = parseRepoUrl(repository);
+    github = `https://github.com/${userName}/${projectName}`;
+  } catch (err) {
+    throw firostError(
+      'NORSKA_THEME_DOCS_NO_REPOSITORY',
+      'Cannot find repository url. Please set your repository key in ./lib/package.json in the "user/project" form'
+    );
+  }
 
   return {
     name,
