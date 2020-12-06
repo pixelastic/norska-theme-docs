771ae50727d06d4b617fcd9ffb9e7fc4a7e01c0f
Tim Carry <tim@pixelastic.com>
2020-12-07 00:34:00 +0100

    feat(scroll): Scroll sidebar to the current page

--- src/script.js
+++ src/script.js
@@ -1,7 +1,17 @@
 const lazyload = require('norska/frontend/lazyload');
 module.exports = {
   async init() {
-    // Lazy loading of images when in viewport
+    this.initSidebar();
+    this.initImages();
+  },
+  // Scroll sidebar to the current page
+  initSidebar() {
+    const link = document.querySelector('.js-navigation-link-active');
+    const sidebar = document.querySelector('.js-scrollable-sidebar');
+    sidebar.scrollTop = link.offsetTop - 100;
+  },
+  // Lazy loading of images when in viewport
+  initImages() {
     lazyload.init();
   },
 };
