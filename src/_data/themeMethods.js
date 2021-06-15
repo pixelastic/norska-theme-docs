721d85a15de119ef11c5052aee6f82ca806b9578
Tim Carry <tim@pixelastic.com>
2021-06-15 22:09:12 +0200

    Use default logo as favicon

--- src/_data/themeMethods.js
+++ src/_data/themeMethods.js
@@ -23,29 +23,6 @@ module.exports = {
     return { title, relativeLink, isActive };
   },
   /**
-   * Returns the HTML code for the header logo, or the path to save it on disk
-   * if not set
-   * @param {object} dependencyInjection Object containing pug methods
-   * Expect include
-   * @returns {string} HTML representation of the logo
-   **/
-  headerLogos(dependencyInjection = {}) {
-    const { include, _ } = dependencyInjection;
-    const filepaths = {
-      default: '_includes/logo.pug',
-      md: '_includes/logo_md.pug',
-      lg: '_includes/logo_lg.pug',
-    };
-
-    return _.mapValues(filepaths, (filepath) => {
-      try {
-        return include(filepath);
-      } catch (_err) {
-        return filepath;
-      }
-    });
-  },
-  /**
    * Returns a URL pointing to the page to edit the file in the GitHub repo
    * @param {object} dependencyInjection Object containing pug data
    * @param {object} dependencyInjection.data Data object
